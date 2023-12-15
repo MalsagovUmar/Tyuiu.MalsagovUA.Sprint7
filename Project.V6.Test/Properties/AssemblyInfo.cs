7f20c9f218a2ed021fdca527d9f68e45d31a977d
Tyuiu.MalsagovUA@outlook.com <mals.umar@yandex.ru>
2023-12-15 20:07:19 +0500

    Add project files.

---
+++ Project.V6.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Project.V6.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Project.V6.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b6bb9581-e251-4840-95ab-b0bbc25a0674")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Project.V6/FormMain.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace Project.V6
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonClientFormMain = new System.Windows.Forms.Button();
+            this.buttonWorkerFormMain = new System.Windows.Forms.Button();
+            this.labelFormMain = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonClientFormMain
+            // 
+            this.buttonClientFormMain.Location = new System.Drawing.Point(326, 256);
+            this.buttonClientFormMain.Name = "buttonClientFormMain";
+            this.buttonClientFormMain.Size = new System.Drawing.Size(104, 59);
+            this.buttonClientFormMain.TabIndex = 0;
+            this.buttonClientFormMain.Text = "Клиент";
+            this.buttonClientFormMain.UseVisualStyleBackColor = true;
+            // 
+            // buttonWorkerFormMain
+            // 
+            this.buttonWorkerFormMain.Location = new System.Drawing.Point(488, 256);
+            this.buttonWorkerFormMain.Name = "buttonWorkerFormMain";
+            this.buttonWorkerFormMain.Size = new System.Drawing.Size(104, 59);
+            this.buttonWorkerFormMain.TabIndex = 0;
+            this.buttonWorkerFormMain.Text = "Работник";
+            this.buttonWorkerFormMain.UseVisualStyleBackColor = true;
+            // 
+            // labelFormMain
+            // 
+            this.labelFormMain.AutoSize = true;
+            this.labelFormMain.Location = new System.Drawing.Point(431, 203);
+            this.labelFormMain.Name = "labelFormMain";
+            this.labelFormMain.Size = new System.Drawing.Size(70, 20);
+            this.labelFormMain.TabIndex = 1;
+            this.labelFormMain.Text = "Кто вы?";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 544);
+            this.Controls.Add(this.labelFormMain);
+            this.Controls.Add(this.buttonWorkerFormMain);
+            this.Controls.Add(this.buttonClientFormMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Поликлиника \"Здоровье+\"";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClientFormMain;
+        private System.Windows.Forms.Button buttonWorkerFormMain;
+        private System.Windows.Forms.Label labelFormMain;
+    }
+}
+
